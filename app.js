e3cee9608ebf777ca3f8cece157d8f85aa79125d
anurag-rastogi332 <rastogianurag675@gmail.com>
2026-02-04 21:42:04 +0530

    Describe your changes

--- app.js
+++ app.js
@@ -37,5 +37,16 @@ app.get('/', (req, res) => res.redirect('/student/dashboard'));
 app.get('/login', (req, res) => res.render('login', { error: null }));
 app.get('/register', (req, res) => res.render('register', { error: null }));
 
+// 404 Handler (Page Not Found)
+app.use((req, res, next) => {
+    res.status(404).send("<h1>404 - Page Not Found</h1><p>The page you are looking for does not exist.</p><p><a href='/'>Go Home</a></p>");
+});
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("<h1>500 - Server Error</h1><p>Something went wrong on the server.</p>");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
